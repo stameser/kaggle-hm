8e1af14fdc9e99cbfd1d74a002c91a0601e59f56
sstamenov <sstamenov@profinit.eu>
2022-04-15 14:19:12 +0200

    add package name to setup py

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
 setup(
     name='kaggle_hm',
     python_requires='>=3.8',
     scripts=[],
+    packages=['kaggle_hm'],
     include_package_data=True
 )
